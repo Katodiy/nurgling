c4d4298f66eef2754162dfe0ceb36aad48f86264
Katodiy <katodiy@yandex.ru>
2023-02-20 01:12:31 +0300

    NAreaRange fix for not selected objects

--- src/nurgling/NAreaRange.java
+++ src/nurgling/NAreaRange.java
@@ -18,11 +18,11 @@ public class NAreaRange extends NAreaRad{
     Color c2;
     double r;
     public NAreaRange(Owner owner, String name, float r, Color color1, Color color2) {
-        super((Gob) owner, r, color1, color2);
+        super((Gob) owner, (NConfiguration.getInstance().rings.get(name).isEnable) ? (float) NConfiguration.getInstance().rings.get(name).size : 0, color1, color2);
         c1 = color1;
         c2 = color2;
         this.r = r;
-        this.ring =  NConfiguration.getInstance().rings.get(name);
+        this.ring = NConfiguration.getInstance().rings.get(name);
     }
 
     @Override
