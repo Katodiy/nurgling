d5a218cd0f4d0e85d6362a8516eebcaaa7700740
Katodiy <katodiy@yandex.ru>
2023-12-13 18:51:33 +0300

    FineryForge Bot fixes

--- src/nurgling/bots/actions/FFAction.java
+++ src/nurgling/bots/actions/FFAction.java
@@ -15,27 +15,27 @@ public class FFAction implements Action
     public Results run(NGameUI gui) throws InterruptedException {
         new Equip ( new NAlias( new ArrayList<> (Arrays.asList ( "smithshammer" )), new ArrayList<String> () ) ).run(gui);
 
-        ArrayList<InContainer> ffs = new ArrayList<>();
-        new FillContainers(new NAlias("dros"), AreasID.dros, new ArrayList<>(), new TakeMaxFromContainers( new NAlias("dros"), AreasID.fineryforge, ffs)).run(gui);
+        while (true) {
+            ArrayList<InContainer> ffs = new ArrayList<>();
+            new FillContainers(new NAlias("dros"), AreasID.dros, new ArrayList<>(), new TakeMaxFromContainers(new NAlias("dros"), AreasID.fineryforge, ffs)).run(gui);
 
-        for(InContainer ff: ffs)
-        {
-            new PathFinder(gui,ff.gob).run();
-            new OpenTargetContainer(ff.gob,  "Finery Forge").run(gui);
-            new TakeMaxFromContainer("Finery Forge", new NAlias ("bloom")).run(gui);
-            if(NUtils.getGameUI().getInventory().getWItems(new NAlias("Bloom", "bloom")).size()>0) {
-                new UseWorkStation(new NAlias("anvil")).run(gui);
-                new Craft("Wrought Iron", new char[]{'c', 'p', 'm', 'w'}).run(gui);
-                new TransferBars().run(gui);
+            for (InContainer ff : ffs) {
+                new PathFinder(gui, ff.gob).run();
+                new OpenTargetContainer(ff.gob, "Finery Forge").run(gui);
+                new TakeMaxFromContainer("Finery Forge", new NAlias("bloom")).run(gui);
+                if (NUtils.getGameUI().getInventory().getWItems(new NAlias("Bloom", "bloom")).size() > 0) {
+                    new UseWorkStation(new NAlias("anvil")).run(gui);
+                    new Craft("Wrought Iron", new char[]{'c', 'p', 'm', 'w'}).run(gui);
+                    new TransferBars().run(gui);
+                }
             }
+
+            new FillContainers(new NAlias("bar-castiron", "Bar of Cast Iron"), AreasID.fineryforge, new ArrayList<>(), new TakeMaxFromContainers(new NAlias("bar-castiron", "Bar of Cast Iron"), AreasID.cast_iron, new ArrayList<>())).run(gui);
+            new FillContainers(new NAlias("bar-castiron", "Bar of Cast Iron"), AreasID.cast_iron, new ArrayList<>()).run(gui);
+
+            new FillFuelFromPiles(2, new NAlias("coal"), new NAlias("fineryforge"), new NAlias("coal"),
+                    AreasID.fineryforge, AreasID.coal, "Finery Forge").run(gui);
+            new LightGob(new NAlias("fineryforge"), AreasID.fineryforge, 8).run(gui);
         }
-
-        new FillContainers(new NAlias ( "bar-castiron" , "Bar of Cast Iron") , AreasID.fineryforge, new ArrayList<>(), new TakeMaxFromContainers( new NAlias ( "bar-castiron" , "Bar of Cast Iron"), AreasID.cast_iron, new ArrayList<>())).run(gui);
-
-        new FillFuelFromPiles(2, new NAlias("coal"), new NAlias("fineryforge"), new NAlias("coal"),
-                AreasID.fineryforge, AreasID.coal, "Finery Forge").run(gui);
-        new LightGob ( new NAlias ( "fineryforge" ), AreasID.fineryforge, 2 ).run ( gui );
-
-        return new Results(Results.Types.SUCCESS);
     }
 }
