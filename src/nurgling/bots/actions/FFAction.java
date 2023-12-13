8948b7fe3123fa3bfa1b4ea9d3143e5a4054a783
Katodiy <katodiy@yandex.ru>
2023-12-13 18:59:13 +0300

    added pause for ff bot

--- src/nurgling/bots/actions/FFAction.java
+++ src/nurgling/bots/actions/FFAction.java
@@ -1,13 +1,17 @@
 package nurgling.bots.actions;
 
+import haven.Coord;
 import haven.Gob;
 import nurgling.*;
 import nurgling.bots.tools.InContainer;
 import nurgling.bots.tools.OutContainer;
 import nurgling.tools.AreasID;
+import nurgling.tools.Finder;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+
+import static haven.OCache.posres;
 
 public class FFAction implements Action
 {
@@ -16,6 +20,21 @@ public class FFAction implements Action
         new Equip ( new NAlias( new ArrayList<> (Arrays.asList ( "smithshammer" )), new ArrayList<String> () ) ).run(gui);
 
         while (true) {
+            while(true)
+            {
+                boolean needBreak = false;
+                for ( Gob gob : Finder.findObjectsInArea ( new NAlias("fineryforge"), Finder.findNearestMark(AreasID.fineryforge) ) ) {
+                    if ((gob.getModelAttribute() & 8) == 0) {
+                        needBreak = true;
+                    }
+                }
+                if(needBreak)
+                    break;
+                Thread.sleep(1000);
+            }
+
+
+
             ArrayList<InContainer> ffs = new ArrayList<>();
             new FillContainers(new NAlias("dros"), AreasID.dros, new ArrayList<>(), new TakeMaxFromContainers(new NAlias("dros"), AreasID.fineryforge, ffs)).run(gui);
 
